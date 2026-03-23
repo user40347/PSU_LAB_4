8ea038bbf83e01c2f58deddc9f0be8ba38c8777b
user40347 <140146157+user40347@users.noreply.github.com>
2026-03-23 09:29:50 +0500

    Добавьте файлы проекта.

---
+++ PSU_LAB_4/Program.fs
@@ -0,0 +1,58 @@
+﻿open System
+
+//Структура ветви
+type Tree<'a> = 
+    | Node of value:'a * children: Tree<'a> list
+
+let rec map f (Node (v, children)) =
+    Node(f v, List.map (map f) children)
+
+// Проверка вводимого целого числа
+let rec readInt (prompt: string) =
+    printf "%s" prompt
+    match System.Console.ReadLine () with
+    | null | "" ->
+        printfn "Ошибка: введите целое число."
+        readInt prompt
+    | s ->
+        match System.Int32.TryParse s with
+        | true, v -> v
+        | _ ->
+            printfn "Ошибка: некорректный формат целого числа."
+            readInt prompt
+
+//Построение дерева
+let rec readFromConsole () : Tree<string> =
+    printf "Введите значение узла: "
+    let value = Console.ReadLine ()
+    let count = readInt "Сколько потомков у этого узла? "
+    let children =
+        [ for _ in 1 .. count do
+            yield readFromConsole () ]
+    Node (value, children)
+
+//Вывод структуры дерева
+let rec printTree indent (Node (v, children)) =
+    printfn  "%s\%dx" (String.replicate (indent) "  ") v
+    children |> List.iter (fun child -> printTree (indent + 2) child)
+
+// Ввод символа
+let rec readChar () =
+    printf "Введите символ, который нужно добавить в конец каждой строки: "
+    let symbolLine = Console.ReadLine ()
+    if not (String.IsNullOrEmpty symbolLine) then symbolLine.[0]
+    else
+        printfn "Пустая строка — повторите ввод."
+        readChar ()
+
+[<EntryPoint>]
+let main argv =
+    printfn "Построим исходное дерево."
+    let tree = readFromConsole ()
+    let char = readChar ()
+    let newTree = map (fun s -> s + string char) tree
+    printfn "\nИсходное дерево :"
+    printTree 0 tree
+    printfn "\nНовое дерево (после добавления '%c'):" char
+    printTree 0 newTree
+    0
